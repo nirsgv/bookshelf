52cba97181453d07fd52faba67d17229f173501d
Nir Segev <nirsegevmail@gmail.com>
2020-10-03 16:55:18 +0300

    add item functionality

---
+++ src/components/addItemButton.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useReducer, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export default function AddItemButton() {
+  const { books, setBooks, user, addBook } = useContext(GlobalContext);
+
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={() =>
+          addBook({
+            BOOK_ID: '2354345g-r34636-40d5-a44b-c0d6c7ad02fe',
+            PRICE: 19,
+            WRITTEN_BY: 'Jay Shetty',
+            PUBLISHED_BY: 'Andromediy',
+            TITLE: 'Think Like a Monk: Train Your Mind for Peace',
+            BEING_INIT: true,
+            __v: 0,
+          })
+        }
+      >
+        +
+      </button>
+    </div>
+  );
+}
